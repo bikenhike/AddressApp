4ba42fe01e57a769aacc583310f359f1d385d114
Marinus <marinus.glaesser@posteo.de>
2016-09-17 12:37:32 +0200

    changed

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,6 +11,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("HelloWorldTest.fxml"));
+
+        root.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
